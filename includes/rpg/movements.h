c801ef7a7785ec1239d4b812e747ed0e930de383
Jose Jonathan <jonathan-emmanuel.jose@epitech.eu>
2020-04-28 13:47:12 +0200

    User collides with small walls but not yet big

--- includes/rpg/movements.h
+++ includes/rpg/movements.h
@@ -13,11 +13,11 @@
 #define SQ_L 400
 #define SQ_R 1300
 #define SQ_U 50
-#define SQ_D 1000
+#define SQ_D 950
 
 /* ------- move.c ------- */
 void move_perso(game_t *game);
-void move_map(game_t *game, sfVector2i wanted);
+int move_map(game_t *game, sfVector2i wanted);
 sfVector2i move_perso_in_map(game_t *game);
 sfVector2f move_in_wanted_zone(sfVector2i wanted);
 
--- sources/map/map.c
+++ sources/map/map.c
@@ -7,8 +7,25 @@
 
 #include "rpg.h"
 
+void collide_walls(game_t *game)
+{
+    sfVector2i m_p = game->map->position;
+    sfVector2f u_p = sfSprite_getPosition(game->animation->perso);
+    char way = game->animation->way;
+
+    if (u_p.x < SQ_L - 30)
+        sfSprite_move(game->animation->perso, (sfVector2f){10, 0});
+    if (u_p.x > SQ_R)
+        sfSprite_move(game->animation->perso, (sfVector2f){-10, 0});
+    if (u_p.y < SQ_U - 30)
+        sfSprite_move(game->animation->perso, (sfVector2f){0, 10});
+    if (u_p.y > SQ_D + 10)
+        sfSprite_move(game->animation->perso, (sfVector2f){0, -10});
+}
+
 void draw_map(game_t *game)
 {
-    move_map(game, move_perso_in_map(game));
+    if (!move_map(game, move_perso_in_map(game)))
+        collide_walls(game);
     sfRenderWindow_drawSprite(game->window, game->map->map, NULL);
 }
