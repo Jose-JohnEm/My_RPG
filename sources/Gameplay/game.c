dd843b60d5502ae843f885d89c269632a998d3be
momo <mohamed1.mrabet@epitech.eu>
2020-04-30 15:50:57 +0200

    adding enemies

---
+++ sources/Gameplay/game.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2019
+** game.c
+** File description:
+** my_rpg
+*/
+
+#include "rpg.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
