c7c63c88a1ffe3e1804ec54d33bc2fee64bdb092
momo <mohamed1.mrabet@epitech.eu>
2020-04-30 19:11:21 +0200

    adding collisions(half)

--- includes/rpg/map_collision.h
+++ includes/rpg/map_collision.h
@@ -42,5 +42,138 @@
                   set_col(0, 1, 4, 5); \
                   set_col(0, 1, 4, 6); \
                   set_col(0, 1, 4, 7); \
+                                       \
+                  set_col(1, 1, 6, 0); \
+                  set_col(1, 1, 6, 1); \
+                  set_col(1, 1, 6, 2); \
+                  set_col(1, 1, 6, 2); \
+                  set_col(1, 1, 7, 2); \
+                  set_col(1, 1, 8, 2); \
+                  set_col(1, 1, 9, 2); \
+                  set_col(1, 1, 6, 7); \
+                  set_col(1, 1, 6, 8); \
+                  set_col(1, 1, 6, 9); \
+                  set_col(1, 1, 7, 6); \
+                  set_col(1, 1, 8, 6); \
+                  set_col(1, 1, 9, 6); \
+                                       \
+                  set_col(1, 0, 3, 3); \
+                  set_col(1, 0, 3, 4); \
+                  set_col(1, 0, 3, 5); \
+                  set_col(1, 0, 3, 6); \
+                  set_col(1, 0, 3, 7); \
+                  set_col(1, 0, 4, 3); \
+                  set_col(1, 0, 5, 3); \
+                  set_col(1, 0, 6, 3); \
+                  set_col(1, 0, 6, 4); \
+                  set_col(1, 0, 6, 5); \
+                  set_col(1, 0, 6, 6); \
+                  set_col(1, 0, 6, 7); \
+                  set_col(1, 0, 5, 7); \
+                  set_col(1, 0, 4, 7); \
+                                       \
+                  set_col(2, 0, 2, 6); \
+                  set_col(2, 0, 2, 7); \
+                  set_col(2, 0, 2, 8); \
+                  set_col(2, 0, 2, 9); \
+                  set_col(2, 0, 3, 6); \
+                  set_col(2, 0, 6, 6); \
+                  set_col(2, 0, 7, 6); \
+                  set_col(2, 0, 8, 6); \
+                  set_col(2, 0, 9, 6); \
+                  set_col(2, 0, 3, 7); \
+                  set_col(2, 0, 3, 8); \
+                  set_col(2, 0, 3, 9); \
+                  set_col(2, 0, 6, 7); \
+                  set_col(2, 0, 6, 8); \
+                  set_col(2, 0, 6, 9); \
+                                       \
+                  set_col(2, 1, 3, 0); \
+                  set_col(2, 1, 3, 1); \
+                  set_col(2, 1, 3, 2); \
+                  set_col(2, 1, 3, 3); \
+                  set_col(2, 1, 3, 6); \
+                  set_col(2, 1, 3, 7); \
+                  set_col(2, 1, 3, 8); \
+                  set_col(2, 1, 3, 9); \
+                  set_col(2, 1, 6, 0); \
+                  set_col(2, 1, 6, 1); \
+                  set_col(2, 1, 6, 2); \
+                  set_col(2, 1, 6, 3); \
+                  set_col(2, 1, 6, 6); \
+                  set_col(2, 1, 6, 7); \
+                  set_col(2, 1, 6, 8); \
+                  set_col(2, 1, 6, 9); \
+                  set_col(2, 1, 0, 3); \
+                  set_col(2, 1, 1, 3); \
+                  set_col(2, 1, 2, 3); \
+                  set_col(2, 1, 6, 3); \
+                  set_col(2, 1, 7, 3); \
+                  set_col(2, 1, 8, 3); \
+                  set_col(2, 1, 9, 3); \
+                  set_col(2, 1, 0, 6); \
+                  set_col(2, 1, 1, 6); \
+                  set_col(2, 1, 2, 6); \
+                  set_col(2, 1, 7, 6); \
+                  set_col(2, 1, 8, 6); \
+                  set_col(2, 1, 9, 6); \
+                                       \
+                  set_col(1, 2, 3, 0); \
+                  set_col(1, 2, 2, 1); \
+                  set_col(1, 2, 2, 2); \
+                  set_col(1, 2, 1, 3); \
+                                       \
+                  set_col(3, 0, 0, 6); \
+                  set_col(3, 0, 1, 6); \
+                  set_col(3, 0, 2, 6); \
+                  set_col(3, 0, 3, 6); \
+                                       \
+                  set_col(3, 1, 3, 0); \
+                  set_col(3, 1, 3, 1); \
+                  set_col(3, 1, 3, 2); \
+                  set_col(3, 1, 3, 3); \
+                  set_col(3, 1, 3, 6); \
+                  set_col(3, 1, 3, 7); \
+                  set_col(3, 1, 3, 8); \
+                  set_col(3, 1, 6, 0); \
+                  set_col(3, 1, 6, 1); \
+                  set_col(3, 1, 6, 2); \
+                  set_col(3, 1, 6, 3); \
+                  set_col(3, 1, 6, 4); \
+                  set_col(3, 1, 6, 5); \
+                  set_col(3, 1, 6, 6); \
+                  set_col(3, 1, 6, 7); \
+                  set_col(3, 1, 6, 8); \
+                  set_col(3, 1, 6, 9); \
+                  set_col(3, 1, 0, 3); \
+                  set_col(3, 1, 1, 3); \
+                  set_col(3, 1, 2, 3); \
+                  set_col(3, 1, 0, 6); \
+                  set_col(3, 1, 1, 6); \
+                  set_col(3, 1, 2, 6); \
+                                       \
+                  set_col(2, 2, 3, 0); \
+                  set_col(2, 2, 3, 1); \
+                  set_col(2, 2, 3, 2); \
+                  set_col(2, 2, 6, 0); \
+                  set_col(2, 2, 6, 1); \
+                  set_col(2, 2, 6, 2); \
+                  set_col(2, 2, 2, 0); \
+                  set_col(2, 2, 2, 2); \
+                  set_col(2, 2, 1, 0); \
+                  set_col(2, 2, 1, 1); \
+                  set_col(2, 2, 1, 2); \
+                  set_col(2, 2, 6, 2); \
+                  set_col(2, 2, 7, 2); \
+                  set_col(2, 2, 8, 2); \
+                  set_col(2, 2, 8, 1); \
+                  set_col(2, 2, 8, 0); \
+
+                                    
+
+                  
+
+                  
+                  
 
 #endif
