dfae8890d2431fc988cc2e0b07c58c69a84f3209
Mehdi <mehdi.zehri@epitech.eu>
2020-05-03 14:07:47 +0200

    edited msg from the pnj

--- sources/pnj/msgbox.c
+++ sources/pnj/msgbox.c
@@ -13,7 +13,6 @@ void init_msgbox(game_t *game)
     game->pnj.boxmsg = sfSprite_create();
     sfSprite_setTexture(game->pnj.boxmsg, game->pnj.t_boxmsg, sfTrue);
     game->pnj.pnjtext = sfText_create();
-    sfText_setString(game->pnj.pnjtext, q_hello);
     sfText_setFont(game->pnj.pnjtext, game->intro.font);
     sfText_setCharacterSize(game->pnj.pnjtext, 30);
     sfText_setColor(game->pnj.pnjtext, sfBlack);
@@ -27,12 +26,15 @@ void select_msg(game_t *game)
         sfText_setString(game->pnj.pnjtext, q_lastkey);
     if (game->pnj.msgtype == 3)
         sfText_setString(game->pnj.pnjtext, q_boss);
-    if (game->pnj.msgtype == 3)
+    if (game->pnj.msgtype == 4)
         sfText_setString(game->pnj.pnjtext, q_boss2);
 }
 
 void draw_msg(game_t *game)
 {
+    if (game->pnj.msgtype == 0)
+        sfText_setString(game->pnj.pnjtext, q_hello);
+    select_msg(game);
     sfText_setPosition(game->pnj.pnjtext, (sfVector2f){500, 850});
     sfSprite_setPosition(game->pnj.boxmsg, (sfVector2f){400, 800});
     sfRenderWindow_drawSprite(game->window, game->pnj.boxmsg, NULL);
