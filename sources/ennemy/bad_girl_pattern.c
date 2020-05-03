0fd5fb1b2a45498e6b88558bfee9925b2d591373
Jose Jonathan <jonathan-emmanuel.jose@epitech.eu>
2020-05-03 13:14:42 +0200

    I fix bad girl's speed

--- sources/ennemy/bad_girl_pattern.c
+++ sources/ennemy/bad_girl_pattern.c
@@ -19,7 +19,7 @@ void check_for_player(g_ennemy *mob, g_anim *user, player_t *p)
     for (int i = 0; i < 4; i++)
         if (s_pos[i].y - 50 > u_pos.y - 100 && s_pos[i].y - 50 < u_pos.y + 100)
             if (s_pos[i].x - 150 > u_pos.x - 100 && s_pos[i].x - 150 < u_pos.x + 100)
-                p->hp -= 3;
+                p->hp -= 2;
 }
 
 void bad_girl_pattern(game_t *g, int i)
@@ -27,23 +27,23 @@ void bad_girl_pattern(game_t *g, int i)
     static int way = -1;
     float time = sfTime_asSeconds(sfClock_getElapsedTime(g->ennemy[i].clock));
 
-    if (way == -1 && time >= 3) {
+    if (way == -1 && time >= 4) {
         sfClock_restart(g->ennemy[i].clock);
         way = (rand() % 4) + 1;
         time = sfTime_asSeconds(sfClock_getElapsedTime(g->ennemy[i].clock));
-    } else if (time < 1) {
+    } else if (time < 2) {
         if (way == DOWN || way == UP) {
             if (way == DOWN)
-                sfSprite_move(g->ennemy[i].mob, (sfVector2f){0, 10});
+                sfSprite_move(g->ennemy[i].mob, (sfVector2f){0, 5});
             if (way == UP)
-                sfSprite_move(g->ennemy[i].mob, (sfVector2f){0, -10});
+                sfSprite_move(g->ennemy[i].mob, (sfVector2f){0, -5});
         } else if (way == RIGHT || way == LEFT) {
             if (way == RIGHT)
-                sfSprite_move(g->ennemy[i].mob, (sfVector2f){10, 0});
+                sfSprite_move(g->ennemy[i].mob, (sfVector2f){5, 0});
             if (way == LEFT)
-                sfSprite_move(g->ennemy[i].mob, (sfVector2f){-10, 0});
+                sfSprite_move(g->ennemy[i].mob, (sfVector2f){-5, 0});
         }
-    } else if (time >= 1) {
+    } else if (time >= 2) {
         way = -1;
         g->ennemy[i] = bad_girl_attack(g->ennemy[i], &g->window);
         check_for_player(&g->ennemy[i], g->animation, &g->player);
--- sources/ennemy/mob_attack.c
+++ sources/ennemy/mob_attack.c
@@ -21,17 +21,17 @@ g_ennemy bad_girl_attack(g_ennemy mob, sfRenderWindow **win)
         sfSprite_setPosition(mob.attack.right, cur_pos);
         ok = 1;
     }
-    sfSprite_move(mob.attack.up, (sfVector2f){0, -20});
-    sfSprite_move(mob.attack.down, (sfVector2f){0, 20});
-    sfSprite_move(mob.attack.right, (sfVector2f){20, 0});
-    sfSprite_move(mob.attack.left, (sfVector2f){-20, 0});
+    sfSprite_move(mob.attack.up, (sfVector2f){0, -15});
+    sfSprite_move(mob.attack.down, (sfVector2f){0, 15});
+    sfSprite_move(mob.attack.right, (sfVector2f){15, 0});
+    sfSprite_move(mob.attack.left, (sfVector2f){-15, 0});
     if (ok == 1) {
         sfRenderWindow_drawSprite(*win, mob.attack.up, NULL);
         sfRenderWindow_drawSprite(*win, mob.attack.down, NULL);
         sfRenderWindow_drawSprite(*win, mob.attack.left, NULL);
         sfRenderWindow_drawSprite(*win, mob.attack.right, NULL);
     }
-    if (sfTime_asSeconds(sfClock_getElapsedTime(mob.clock)) > 2.95)
+    if (sfTime_asSeconds(sfClock_getElapsedTime(mob.clock)) > 3.95)
         ok = 0;
     return mob;
 }
